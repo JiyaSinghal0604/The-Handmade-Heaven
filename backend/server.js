911acb1fbe52525c860120b86260706abcc880eb
JiyaSinghal0604 <jiya.singhal513651@gmail.com>
2026-07-23 18:55:18 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', 
-    'https://the-handmade-heaven-frontend.onrender.com/'
+    'https://the-handmade-heaven-frontend.onrender.com'
   ],
   credentials: true
 }));
